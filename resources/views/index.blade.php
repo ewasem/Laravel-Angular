ff8c56a5cfedc31f478a139af004c21fbb8b2c5e
Eduardo Wasem <eduardowasem@outlook.com>
2022-04-20 07:48:42 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <base href="/">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ asset('js/styles.10a3678fc5e4410f.css') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('js/favicon.ico') }}">
+
+    </head>
+    <body>
+        <app-root></app-root>
+        <script src="{{ asset('js/runtime.816becf396788283.js') }}" type="module"></script>
+        <script src="{{ asset('js/polyfills.f0857a049ddc3d8b.js') }}" type="module"></script>
+        <script src="{{ asset('js/main.8cb2c89e029d918b.js') }}" type="module"></script>
+
+    </body>
+</html>
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostControlador;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/', [PostControlador::class, 'index']);
+
+Route::post('/', [PostControlador::class, 'store']);
+
+Route::delete('/{id}', [PostControlador::class, 'destroy']);
+
+Route::get('/like/{id}', [PostControlador::class, 'like']);
